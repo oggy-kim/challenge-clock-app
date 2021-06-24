b6d0ace6fb19f3e13c01cf2f4ac92a4649fe2072
Oggy Kim <oksamha@gmail.com>
2021-06-24 22:45:49 +0900

    modify: change API temporarily(too many requests)

--- src/api/geolocation.ts
+++ src/api/geolocation.ts
@@ -4,6 +4,11 @@
 import axios from 'axios';
 
 export const getMyPosition = async (ip: string) => {
-    const { data: { latitude, longitude, region, country }} = await axios.get(`https://ipapi.co/${ip}/json`);
-    return {latitude, longitude,region, country };
-}
+  // api 과다 요청 방지를 위한 임시값 설정
+  // const { data: { latitude, longitude, region, country }} = await axios.get(`https://ipapi.co/${ip}/json`);
+  const latitude = 37;
+  const longitude = 127;
+  const region = 'seoul';
+  const country = 'KR';
+  return { latitude, longitude, region, country };
+};
